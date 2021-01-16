9de56ebfeb0d8e5f41e6d627125a40b7fff7dd02
AudreyNanou <71023317+AudreyNanou@users.noreply.github.com>
2021-01-16 18:29:16 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+ document.querySelector('#monImage1').addEventListener('click', function() {
+            document.getElementById('monImage0').src = "sideshow/grd/granola-1.jpg";
+            document.getElementById('monImage0').alt = "granola-logo-1";
+
+        });
+
+        document.querySelector('#monImage2').addEventListener('click', function() {
+            document.getElementById('monImage0').src = "sideshow/grd/granola-2.jpg";
+            document.getElementById('monImage0').alt = "granola-logo-2";
+        });
+        document.querySelector('#monImage3').addEventListener('click', function() {
+            document.getElementById('monImage0').src = "sideshow/grd/granola-3.jpg";
+            document.getElementById('monImage0').alt = "granola-logo-3";
+        });
+        document.querySelector('#monImage4').addEventListener('click', function() {
+            document.getElementById('monImage0').src = "sideshow/grd/vg-1.jpg";
+            document.getElementById('monImage0').alt = "Decapeetdecrocs";
+
+        });
+        document.querySelector('#monImage5').addEventListener('click', function() {
+            document.getElementById('monImage0').src = "sideshow/grd/vg-2.jpg";
+            document.getElementById('monImage0').alt = "Jeremiah";
+
+        });
+        document.querySelector('#monImage6').addEventListener('click', function() {
+            document.getElementById('monImage0').src = "sideshow/grd/vg-3.jpg";
+            document.getElementById('monImage0').alt = "Saiyuki";
+
+        });
+        document.querySelector('#monImage7').addEventListener('click', function() {
+            document.getElementById('monImage0').src = "sideshow/grd/daku.jpg";
+            document.getElementById('monImage0').alt = "Santiag";
+
+        });
+        document.querySelector('#monImage8').addEventListener('click', function() {
+            document.getElementById('monImage0').src = "sideshow/grd/musique.jpg";
+            document.getElementById('monImage0').alt = "Spirou";
+
+        });
+        document.querySelector('#monImage9').addEventListener('click', function() {
+            document.getElementById('monImage0').src = "sideshow/grd/blob.jpg";
+            document.getElementById('monImage0').alt = "Trollsdettroy";
+
+        });
